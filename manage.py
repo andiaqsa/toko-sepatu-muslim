83b96a834cda298169a8b889ff6920b2e6e6c141
ANDI AQSA MAPPATUNRU MARZUKI <andiaqsa365@gmail.com>
2024-09-25 09:27:34 +0700

    aqsaa

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Toko_Sepatu_Sunnah.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toko_sepatu_muslim.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
